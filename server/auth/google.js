2e488857cdecb70fbbb1dc7463e4d7bdc1e3222f
Anna Medyukh <annamedyukh@gmail.com>
2018-02-08 01:05:30 -0500

    setting up auth :camel:

---
+++ server/auth/google.js
@@ -0,0 +1,7 @@
+const passport = require('passport')
+const router = require('express').Router()
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const {User} = require('../db/models')
+module.exports = router
+
+
