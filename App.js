043ea2006f13cec96e47f24b62dacd0d4e7a995e
MissAnichka <annamedyukh@gmail.com>
2019-03-15 15:19:38 -0400

    adding font locally, waiting to load, reorganized

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, Alert, Linking } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StatusBar, StyleSheet, Text, View, Image, TextInput, Button, Alert, Linking } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
 import { Router, Scene } from 'react-native-router-flux';
 
 import Home from './client/components/Home.js';
@@ -11,17 +12,48 @@ import Map from './client/components/Map.js';
 
 
 export default class App extends Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
   render() {
-    return (
-      <Router>
-        <Scene key="root" hideNavBar={true}>
-          <Scene key="home" component={Home}/>
-          <Scene key="trips" component={Trips}/>
-          <Scene key="login" component={Login}/>
-          <Scene key="profile" component={Profile}/>
-          <Scene key="map" component={Map}/>
-        </Scene>
-      </Router>
-    );
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <Router>
+          <Scene key="root" hideNavBar={true}>
+            <Scene key="home" component={Home} />
+            <Scene key="trips" component={Trips} />
+            <Scene key="login" component={Login} />
+            <Scene key="profile" component={Profile} />
+            <Scene key="map" component={Map} />
+          </Scene>
+        </Router>
+      );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        ...Icon.Ionicons.font,
+        'Roboto': require('./assets/font/Roboto-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
